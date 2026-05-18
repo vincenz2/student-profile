bba71cff4ef0bd21e0c18c989abcf3a7c3a3dde6
vincenz2 <vincentgarciaa01@gmail.com>
2026-05-18 08:08:07 +0800

    first upload

---
+++ app/Layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-app>
+        <v-navigation-drawer v-model="drawer">
+            <v-list>
+                <v-list-item
+                    prepend-icon="mdi-account-plus"
+                    title="Add Student"
+                    value="addstud">
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+
+        <v-app-bar>
+            <v-app-bar-nav-icon
+                @click="drawer = !drawer"
+            ></v-app-bar-nav-icon>
+
+            <v-app-bar-title>Student Management</v-app-bar-title>
+        </v-app-bar>
+
+        <v-main>
+            <NuxtPage />
+        </v-main>
+    </v-app>
+</template> 
+
+<script>
+    import { ref } from 'vue'
+
+    export default {
+        data: () => ({ drawer: null}),
+    }
+
+    const drawer = ref(null)
+</script>
---
+++ app/pages/index.vue
@@ -0,0 +1,107 @@
+<template>
+    <v-app>
+        <v-main>
+            <v-container>
+                <v-row>
+
+                    <v-col
+                        cols="12">
+
+                        <v-text-field
+                            label="Student no"
+                            variant="outlined"
+                            placeholder="24-00328">
+                        </v-text-field>
+                    </v-col>
+
+                    <v-col
+                        cols="12"
+                        md="4">
+
+                        <v-text-field
+                            label="Last Name"
+                            variant="outlined"
+                            placeholder="Garcia">
+                        </v-text-field>
+                    </v-col>
+
+                    <v-col
+                        cols="12"
+                        md="4">
+
+                        <v-text-field
+                            label="First Name"
+                            variant="outlined"
+                            placeholder="Vincent">
+                        </v-text-field>
+                    </v-col>
+
+                    <v-col
+                        cols="12"
+                        md="4">
+
+                        <v-text-field
+                            label="Middle Name"
+                            variant="outlined"
+                            placeholder="Canlas">
+                        </v-text-field>
+                    </v-col>
+
+                    <v-col
+                        cols="12"
+                        md="4">
+
+                        <v-combobox
+                            label="Gender"
+                            variant="outlined"
+                            :items="['Male','Female','LGBTQ']"
+                            clearable>
+                        </v-combobox>
+                    </v-col>
+                    
+                    <v-col
+                        cols="12"
+                        md="4">
+
+                        <v-text-field
+                            label="Age"
+                            variant="outlined"
+                            placeholder="18 Years Old">
+                        </v-text-field>
+                     </v-col>
+                </v-row>
+
+                    <v-btn 
+                        prepend-icon="mdi mdi-tag-edit-outline"
+                        variant="outlined"
+                        color="green">
+                        Edit
+                    </v-btn>
+
+
+                    <v-btn 
+                        prepend-icon="mdi mdi-update"
+                        variant="outlined"
+                        color="blue">
+                        Update
+                    </v-btn>
+
+                   
+                    <v-btn 
+                        prepend-icon="mdi mdi-content-save-all-outline"
+                        variant="outlined"
+                        color="orange">
+                        Save
+                    </v-btn>
+
+
+                    <v-btn 
+                        prepend-icon="mdi mdi-delete-alert-outline"
+                        variant="outlined"
+                        color="red">
+                        Delete
+                    </v-btn>
+            </v-container>
+        </v-main>
+    </v-app>
+</template>
