7cc2ec0cc58bece8efe34ff53c6a87e0e16df902
vincenz2 <vincentgarciaa01@gmail.com>
2026-05-18 10:22:05 +0800

    cms and web integrate

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,7 +8,8 @@
                         cols="12">
 
                         <v-text-field
-                            label="Student no"
+                            label="Student No."
+                            prepend-inner-icon="mdi mdi-numeric"
                             variant="solo-inverted"
                             placeholder="24-00328">
                         </v-text-field>
@@ -20,6 +21,7 @@
 
                         <v-text-field
                             label="Last Name"
+                            prepend-inner-icon="mdi-account-box-outline"
                             variant="solo-inverted"
                             placeholder="Garcia">
                         </v-text-field>
@@ -31,6 +33,7 @@
 
                         <v-text-field
                             label="First Name"
+                            prepend-inner-icon="mdi-account-box-outline" 
                             variant="solo-inverted"
                             placeholder="Vincent">
                         </v-text-field>
@@ -42,6 +45,7 @@
 
                         <v-text-field
                             label="Middle Name"
+                            prepend-inner-icon="mdi-account-box-outline" 
                             variant="solo-inverted"
                             placeholder="Canlas">
                         </v-text-field>
@@ -53,6 +57,7 @@
 
                         <v-combobox
                             label="Course"
+                            prepend-inner-icon="mdi-account-details-outline" 
                             variant="solo-inverted"
                             :items="['BSCS','DCT',]"
                             clearable>
@@ -66,6 +71,7 @@
 
                         <v-combobox
                             label="Year"
+                            prepend-inner-icon="mdi-account-details-outline" 
                             variant="solo-inverted"
                             :items="['1st year','2nd year','3rd year','4th year']"
                             clearable>
@@ -78,6 +84,7 @@
 
                         <v-combobox
                             label="Section"
+                            prepend-inner-icon="mdi-account-details-outline" 
                             variant="solo-inverted"
                             :items="['A','B','C']"
                             clearable>
@@ -90,6 +97,7 @@
 
                         <v-text-field
                             label="Address"
+                            prepend-inner-icon="mdi-map-marker-outline"
                             variant="solo-inverted"
                             placeholder="Laput, Mexico, Pampanga">
                         </v-text-field>
@@ -101,6 +109,7 @@
 
                         <v-text-field
                             label="Contact No."
+                            prepend-inner-icon="mdi-card-account-phone-outline"
                             variant="solo-inverted"
                             placeholder="09233453123">
                         </v-text-field>
@@ -112,8 +121,9 @@
 
                         <v-combobox
                             label="Gender"
-                            variant="solo-inverted"
+                            prepend-inner-icon="mdi mdi-gender-transgender"
                             :items="['Male','Female',' lGBTQ']"
+                            variant="solo-inverted"
                             clearable>
                         </v-combobox>
                     </v-col>
@@ -146,7 +156,84 @@
                         color="red">
                         Delete
                     </v-btn>
+
+
+                    <v-card
+                        title="Student Profile"
+                        flat>
+
+                         <v-text-field
+                            v-model="search"
+                            label="Search"
+                            prepend-inner-icon="mdi-magnify"
+                            variant="outlined"
+                            hide-details
+                            single-line
+                        ></v-text-field>
+
+                        <v-data-table
+                            :headers="titlecolum"
+                            :items="nilalamanngcolum"
+                            :search="search"
+                        ></v-data-table>
+                    </v-card>
             </v-container>
         </v-main>
     </v-app>
 </template>
+
+<script setup>
+ const search = ref('')
+
+const titlecolum = [
+    {
+        key: 'stuno', 
+        title: 'Student Name'
+    
+    },
+
+    {
+        key: 'lname', 
+        title: 'Last Name'
+    
+    },
+
+    {
+        key: 'fname', 
+        title: 'First Name'
+    
+    },
+
+    {
+        key: 'mname', 
+        title: 'Middle Name'
+    
+    },
+
+    {
+        key: 'ylevel', 
+        title: 'Year Level'
+    
+    },
+
+     {
+        key: 'csection', 
+        title: 'Section'
+    
+    },
+]
+
+const  nilalamanngcolum = [
+    {
+        stuno: '2400328', 
+        lname: 'Garcia',
+        fname: 'Vincent'
+    },
+
+    {
+        stuno: '2400328', 
+        lname: 'Garcia',
+        fname: 'Vincent'
+    },
+]
+</script>
