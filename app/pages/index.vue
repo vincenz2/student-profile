3bdc2a393759e178f801213c82e6b36433a47db3
vincenz2 <vincentgarciaa01@gmail.com>
2026-05-18 09:07:20 +0800

    student textfields

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -9,7 +9,7 @@
 
                         <v-text-field
                             label="Student no"
-                            variant="outlined"
+                            variant="solo-inverted"
                             placeholder="24-00328">
                         </v-text-field>
                     </v-col>
@@ -20,7 +20,7 @@
 
                         <v-text-field
                             label="Last Name"
-                            variant="outlined"
+                            variant="solo-inverted"
                             placeholder="Garcia">
                         </v-text-field>
                     </v-col>
@@ -31,7 +31,7 @@
 
                         <v-text-field
                             label="First Name"
-                            variant="outlined"
+                            variant="solo-inverted"
                             placeholder="Vincent">
                         </v-text-field>
                     </v-col>
@@ -42,7 +42,7 @@
 
                         <v-text-field
                             label="Middle Name"
-                            variant="outlined"
+                            variant="solo-inverted"
                             placeholder="Canlas">
                         </v-text-field>
                     </v-col>
@@ -52,9 +52,34 @@
                         md="4">
 
                         <v-combobox
-                            label="Gender"
-                            variant="outlined"
-                            :items="['Male','Female','LGBTQ']"
+                            label="Course"
+                            variant="solo-inverted"
+                            :items="['BSCS','DCT',]"
+                            clearable>
+                        </v-combobox>
+                    </v-col>
+
+                    
+                    <v-col
+                        cols="12"
+                        md="4">
+
+                        <v-combobox
+                            label="Year"
+                            variant="solo-inverted"
+                            :items="['1st year','2nd year','3rd year','4th year']"
+                            clearable>
+                        </v-combobox>
+                    </v-col>
+
+                    <v-col
+                        cols="12"
+                        md="4">
+
+                        <v-combobox
+                            label="Section"
+                            variant="solo-inverted"
+                            :items="['A','B','C']"
                             clearable>
                         </v-combobox>
                     </v-col>
@@ -64,20 +89,42 @@
                         md="4">
 
                         <v-text-field
-                            label="Age"
-                            variant="outlined"
-                            placeholder="18 Years Old">
+                            label="Address"
+                            variant="solo-inverted"
+                            placeholder="Laput, Mexico, Pampanga">
                         </v-text-field>
                      </v-col>
-                </v-row>
 
+                      <v-col
+                        cols="12"
+                        md="4">
+
+                        <v-text-field
+                            label="Contact No."
+                            variant="solo-inverted"
+                            placeholder="09233453123">
+                        </v-text-field>
+                     </v-col>
+
+                     <v-col
+                        cols="12"
+                        md="4">
+
+                        <v-combobox
+                            label="Gender"
+                            variant="solo-inverted"
+                            :items="['Male','Female',' lGBTQ']"
+                            clearable>
+                        </v-combobox>
+                    </v-col>
+
+                </v-row>
                     <v-btn 
                         prepend-icon="mdi mdi-tag-edit-outline"
                         variant="outlined"
                         color="green">
                         Edit
                     </v-btn>
-
 
                     <v-btn 
                         prepend-icon="mdi mdi-update"
@@ -86,14 +133,12 @@
                         Update
                     </v-btn>
 
-                   
                     <v-btn 
                         prepend-icon="mdi mdi-content-save-all-outline"
                         variant="outlined"
                         color="orange">
                         Save
                     </v-btn>
-
 
                     <v-btn 
                         prepend-icon="mdi mdi-delete-alert-outline"
